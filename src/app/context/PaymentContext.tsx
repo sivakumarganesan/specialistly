3b5cdff10f5e1ac26ab1e35312ac5365dfcb950b
siva-s3innovation <siva-s3innovations@hotmail.com>
2026-02-28 08:03:17 -0800

    Debug: Add try-catch to openPayment to catch any silent errors

--- src/app/context/PaymentContext.tsx
+++ src/app/context/PaymentContext.tsx
@@ -33,16 +33,22 @@ export const PaymentProvider: React.FC<{ children: React.ReactNode }> = ({ child
   }, [isOpen, paymentConfig]);
 
   const openPayment = useCallback((config: PaymentConfig) => {
-    console.log('[PaymentContext] openPayment called with config:', {
-      serviceId: config.serviceId,
-      serviceType: config.serviceType,
-      serviceName: config.serviceName,
-    });
-    console.log('[PaymentContext] About to call setPaymentConfig');
-    setPaymentConfig(config);
-    console.log('[PaymentContext] About to call setIsOpen(true)');
-    setIsOpen(true);
-    console.log('[PaymentContext] Called setIsOpen(true) - state update queued');
+    try {
+      console.log('[PaymentContext] openPayment called with config:', {
+        serviceId: config.serviceId,
+        serviceType: config.serviceType,
+        serviceName: config.serviceName,
+      });
+      console.log('[PaymentContext] About to call setPaymentConfig');
+      setPaymentConfig(config);
+      console.log('[PaymentContext] setPaymentConfig called');
+      console.log('[PaymentContext] About to call setIsOpen(true)');
+      setIsOpen(true);
+      console.log('[PaymentContext] setIsOpen(true) called - state update queued');
+    } catch (error) {
+      console.error('[PaymentContext] ERROR in openPayment:', error);
+      throw error;
+    }
   }, []);
 
   const closePayment = useCallback(() => {
