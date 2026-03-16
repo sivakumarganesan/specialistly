9cad2190d843c5f2aa1d51240d22dc0736658b00
siva-s3innovation <siva-s3innovations@hotmail.com>
2026-03-15 19:50:36 -0700

    Increase body parser limit to 50mb for course thumbnails

--- backend/server.js
+++ backend/server.js
@@ -113,8 +113,8 @@ app.post(
   }
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Connect to MongoDB
 connectDB();
